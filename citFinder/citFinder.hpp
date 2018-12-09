e17cd0c066b540ceadf69ac88d1e7a3bad81e7e9
mauraisa <aaron.maurais@bc.edu>
2018-12-09 18:10:49 -0500

    Adding citFinder folder.

---
+++ citFinder/citFinder.hpp
@@ -0,0 +1,13 @@
+//
+//  citFinder.hpp
+//  ms2_anotator
+//
+//  Created by Aaron Maurais on 12/9/18.
+//  Copyright © 2018 Aaron Maurais. All rights reserved.
+//
+
+#ifndef citFinder_hpp
+#define citFinder_hpp
+
+
+#endif /* citFinder_hpp */
---
+++ citFinder/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  citFinder
+//
+//  Created by Aaron Maurais on 12/9/18.
+//  Copyright © 2018 Aaron Maurais. All rights reserved.
+//
+
+#include "citFinder.hpp"
+
+int main(int argc, const char** argv)
+{
+	
+	return 0;
+}
