dd54c7f018be02f2d99253ac15c6882626da5c4e
mauraisa <aaron.maurais@bc.edu>
2018-12-09 18:06:11 -0500

    Adding xcode project to vcs.

---
+++ citFinder/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  citFinder
+//
+//  Created by Aaron Maurais on 12/9/18.
+//  Copyright © 2018 Aaron Maurais. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+	return 0;
+}
