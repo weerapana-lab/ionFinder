68c368358d98ceaad4dcf0d82acc364af099f85e
mauraisa <aaron.maurais@bc.edu>
2019-01-03 11:44:09 -0500

    Delete gitVersion.hpp

--- include/gitVersion.hpp
+++
@@ -1,8 +0,0 @@
-
-#ifndef gitVersion_hpp
-#define gitVersion_hpp
-
-const char* GIT_DATE = "Fri Nov 9 18:21:58 2018";
-const char* GIT_COMMIT = "833e295be489be993922f36e3d344251c28ff672";
-
-#endif /* gitVersion_hpp */
