85faf795d05ec1e9bf979dd17b86f984e37a524b
mauraisa <aaron.maurais@bc.edu>
2020-04-02 11:54:59 -0400

    Adding maxquant_constants.

---
+++ python/parsers/modules/maxquant_constants.py
@@ -0,0 +1,14 @@
+# This file was generated automatically by scripts/configure_file
+
+# maxquant file headers 
+SCORE = 'score'
+MSMS_SCAN_NUMBER = 'msms_scan_number'
+MZ = 'mz'
+CHARGE = 'charge'
+EXPERIMENT = 'experiment'
+RAW_FILE = 'raw_file'
+PROTEIN_NAMES = 'protein_names'
+GENE_NAMES = 'gene_names'
+LEADING_PROTEINS = 'leading_proteins'
+MODIFIED_SEQUENCE = 'modified_sequence'
+
