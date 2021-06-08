90845bd65cd2fb521b269f533b2e732294a83455
Aaron Maurais <mauraisa@bc.edu>
2021-06-08 10:44:11 -0400

    Add --fileExt option to both parsers.

--- python/parsers/parse_maxquant.py
+++ python/parsers/parse_maxquant.py
@@ -139,9 +139,6 @@ def main():
                                      epilog="parse_maxquant was written by Aaron Maurais.\n"
                                             "Email questions or bugs to aaron.maurais@bc.edu")
 
-    parser.add_argument('--fileExt', default='mzML',
-                        help='File extension to add to MS file names. Default is "mzML".')
-
     parser.add_argument('-g', '--groupMethod', choices=[0, 1, 2], default=1, type=int,
                         help='How many spectra per peptide? 0: include all scans, '
                              '1: Only show the best spectra per sequence, file and charge state, '
